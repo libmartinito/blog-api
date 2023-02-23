9a709236d9f99109bc46cec84ab0920df40c51c3
Lib Martinito <lib.martinito@pm.me>
2023-02-23 23:16:42 +0800

    reset initial environment

--- src/routes/auth.route.ts
+++
@@ -1,7 +0,0 @@
-import express, { Router } from 'express';
-import * as auth from '../controllers/auth.controller';
-
-export const router: Router = express.Router();
-
-router.post('/v1/register', auth.register);
-router.post('/v1/login', auth.login);
