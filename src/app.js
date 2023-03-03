a6069377052a15e1c855aa30fd2587066dbac528
Lib Martinito <lib.martinito@pm.me>
2023-03-03 20:31:37 +0800

    feat: added app.ts and server.ts

---
+++ src/app.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import { NODE_ENV, PORT } from './config'
+import compression from 'compression'
+import errorMiddleware from './middlewares/error.middleware'
+
+class App {
+
+    constructor(routes) {
+        this.app = express()
+        this.env = NODE_ENV || 'development'
+        this.port = PORT || 3000
+
+        this.initializeMiddlewares()
+        this.initializeRoutes(routes)
+        this.initializeErrorHandling()
+    }
+
+    static listen() {
+        this.app.listen(this.port, () => {
+            console.log(`ENV: ${ this.env }`)
+            console.log(`App listening on port: ${ this.port }`)
+        })
+    }
+
+    initializeMiddlewares() {
+        this.app.use(compression())
+        this.app.use(express.json())
+        this.app.use(express.urlencoded({  extended: true }))
+    }
+
+    initializeRoutes(routes) {
+        routes.forEach(route => {
+            this.app.use('/', route.router)
+        })
+    }
+
+    initializeErrorHandling() {
+        this.app.use(errorMiddleware)
+    }
+}
+
+export default App
